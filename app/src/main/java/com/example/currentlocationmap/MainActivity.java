c3f1275a4e2240a4e4d7adb6d88c9e25727ce717
Narasimha <narasimha.y219@gmail.com>
2020-12-12 06:39:18 +0530

    This project aim to find two locations distancs using google maps in android

--- app/src/main/java/com/example/currentlocationmap/MainActivity.java
+++ app/src/main/java/com/example/currentlocationmap/MainActivity.java
@@ -10,7 +10,7 @@ import com.example.currentlocationmap.Services.ServicesClass;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    Button second_button_id,services_button_id,broadcast_button_id;
+    Button second_button_id,services_button_id,broadcast_button_id,diections_button_id;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,10 +18,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
          second_button_id = findViewById(R.id.second_button_id);
          services_button_id = findViewById(R.id.services_button_id);
-        broadcast_button_id = findViewById(R.id.broadcast_button_id);
-        second_button_id.setOnClickListener(this);
-        services_button_id.setOnClickListener(this);
-        broadcast_button_id.setOnClickListener(this);
+         broadcast_button_id = findViewById(R.id.broadcast_button_id);
+        diections_button_id = findViewById(R.id.diections_button_id);
+         second_button_id.setOnClickListener(this);
+         services_button_id.setOnClickListener(this);
+         broadcast_button_id.setOnClickListener(this);
+        diections_button_id.setOnClickListener(this);
 
     }
 
@@ -33,8 +35,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }else if(view == services_button_id){
             Intent intent = new Intent(getApplicationContext(), ServicesClass.class);
             startActivity(intent);
-        }else if (view == services_button_id){
-
+        }else if (view == diections_button_id){
+            Intent intent = new Intent(getApplicationContext(),MapDirectionActivity.class);
+            startActivity(intent);
         }
     }
 }
