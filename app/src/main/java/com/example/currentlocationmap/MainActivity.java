952c5baf58f639e9080d1d063c53641dc0bef9c5
Narasimha <narasimha.y219@gmail.com>
2020-12-13 21:03:05 +0530

    place picker on map

--- app/src/main/java/com/example/currentlocationmap/MainActivity.java
+++ app/src/main/java/com/example/currentlocationmap/MainActivity.java
@@ -6,11 +6,14 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.example.currentlocationmap.CustomePlacePicker.CustomPlacePickerActivity;
+import com.example.currentlocationmap.PlacePicker.PlacePickerActivity;
 import com.example.currentlocationmap.Services.ServicesClass;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    Button second_button_id,services_button_id,broadcast_button_id,diections_button_id;
+    Button second_button_id,services_button_id,carAnimation_button_id,
+            diections_button_id,custome_picker_button_id,place_picker_button_id;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,12 +21,17 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
          second_button_id = findViewById(R.id.second_button_id);
          services_button_id = findViewById(R.id.services_button_id);
-         broadcast_button_id = findViewById(R.id.broadcast_button_id);
+        carAnimation_button_id = findViewById(R.id.carAnimation_button_id);
         diections_button_id = findViewById(R.id.diections_button_id);
+        custome_picker_button_id = findViewById(R.id.custome_picker_button_id);
+        place_picker_button_id = findViewById(R.id.place_picker_button_id);
+
          second_button_id.setOnClickListener(this);
          services_button_id.setOnClickListener(this);
-         broadcast_button_id.setOnClickListener(this);
+        carAnimation_button_id.setOnClickListener(this);
         diections_button_id.setOnClickListener(this);
+        custome_picker_button_id.setOnClickListener(this);
+        place_picker_button_id.setOnClickListener(this);
 
     }
 
@@ -37,6 +45,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             startActivity(intent);
         }else if (view == diections_button_id){
             Intent intent = new Intent(getApplicationContext(),MapDirectionActivity.class);
+            startActivity(intent);
+        }else if (view == carAnimation_button_id){
+            Intent intent = new Intent(getApplicationContext(),CarRootFindingMap.class);
+            startActivity(intent);
+        }else if (view == custome_picker_button_id){
+            Intent intent = new Intent(getApplicationContext(),CustomPlacePickerActivity.class);
+            startActivity(intent);
+        }else if (view == place_picker_button_id){
+            Intent intent = new Intent(getApplicationContext(), PlacePickerActivity.class);
             startActivity(intent);
         }
     }
